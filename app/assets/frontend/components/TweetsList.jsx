41121c876b32c1ccb3ea994b68fca08990129d62
beeflamian <beeflamian@gmail.com>
2016-01-05 11:37:35 -0800

    materialize

---
+++ app/assets/frontend/components/TweetsList.jsx
@@ -0,0 +1,27 @@
+export default class TweetsList extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul className="collection">
+          <li className="collection-item avatar">
+          <i className="material-icons circle">person_pin</i>
+          <span className="title">Alan</span>
+          <p>My #First Tweet</p>
+          </li>
+
+          <li className="collection-item avatar">
+          <i className="material-icons circle">person_pin</i>
+          <span className="title">Alan</span>
+          <p>My #First Tweet</p>
+          </li>
+
+          <li className="collection-item avatar">
+          <i className="material-icons circle">person_pin</i>
+          <span className="title">Alan</span>
+          <p>My #First Tweet</p>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
---
+++ app/assets/frontend/main.jsx
@@ -0,0 +1,22 @@
+import TweetBox from "./components/TweetBox" 
+import TweetsList from "./components/TweetsList"
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <TweetBox />
+        <TweetsList />
+      </div>
+    );
+  }
+}
+
+let documentReady = () => {
+  ReactDOM.render(
+    <Main />,
+    document.getElementById('react')
+  );
+};
+
+$(documentReady);
